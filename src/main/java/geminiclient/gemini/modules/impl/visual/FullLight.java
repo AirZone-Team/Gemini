f8fb0f23e40cf82e98ca56c2c81c54ffc47b9d48
MatrixU5er <89892616+MatrixU5er@users.noreply.github.com>
2025-10-06 20:18:20 +0800

    修复夜视时长,与开关问题

--- src/main/java/geminiclient/gemini/modules/impl/visual/FullLight.java
+++ src/main/java/geminiclient/gemini/modules/impl/visual/FullLight.java
@@ -2,21 +2,33 @@ package geminiclient.gemini.modules.impl.visual;
 
 import geminiclient.gemini.modules.Module;
 import geminiclient.gemini.modules.ModuleEnum;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 
 public class FullLight extends Module {
     public FullLight() {
-        super("FullLight",ModuleEnum.Visual);
+        super("FullLight", ModuleEnum.Visual);
     }
 
     @Override
     public void onEnabled() {
-        if (player != null) {
-            MobEffectInstance instance = new MobEffectInstance(MobEffects.NIGHT_VISION,999,1,false,true,true);
-            player.removeEffect(MobEffects.NIGHT_VISION);
+        if (player != null && !player.hasEffect(MobEffects.NIGHT_VISION)) {
+            MobEffectInstance instance = new MobEffectInstance(
+                    MobEffects.NIGHT_VISION,
+                    Integer.MAX_VALUE,
+                    0, // LEVEL 1
+                    false, // ambient
+                    false, // showParticles
+                    true // showIcon
+            );
             player.addEffect(instance);
+        }
+    }
+
+    @Override
+    public void onDisabled() {
+        if (player != null && player.hasEffect(MobEffects.NIGHT_VISION)) {
+            player.removeEffect(MobEffects.NIGHT_VISION);
         }
     }
 }
