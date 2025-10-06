40181c167ce6f9f41fbd5f17be57a56d6cde2d0d
XeContrast <1683481541@qq.com>
2025-10-06 22:13:39 +0800

    SaveConfigCommand

---
+++ src/main/java/geminiclient/gemini/commands/impl/SaveConfig.java
@@ -0,0 +1,30 @@
+package geminiclient.gemini.commands.impl;
+
+import geminiclient.gemini.Gemini;
+import geminiclient.gemini.commands.Command;
+import geminiclient.gemini.utils.ClientUtils;
+
+public class SaveConfig extends Command {
+    @Override
+    public void onCommand(String message) {
+        String[] args = message.split(" ");
+
+        if (args[0].equalsIgnoreCase(".config")) {
+            if (args.length == 2) {
+                if (args[1].equalsIgnoreCase("save")) {
+                    Gemini.fileSystem.saveConfig();
+                    ClientUtils.addChatMessage(String.format("Saving Config name: %s",Gemini.lastConfigName));
+                } else if (!args[1].equalsIgnoreCase("load")) {
+                    ClientUtils.addChatMessage(".config save");
+                }
+            } else if (args.length == 3) {
+                if (args[1].equalsIgnoreCase("save")) {
+                    Gemini.fileSystem.saveConfig(args[2]);
+                    ClientUtils.addChatMessage(String.format("Saving Config name: %s",args[2]));
+                } else if (!args[1].equalsIgnoreCase("load")){
+                    ClientUtils.addChatMessage(".config save <ConfigName>");
+                }
+            }
+        }
+    }
+}
